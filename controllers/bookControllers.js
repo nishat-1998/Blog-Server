1d374bdfe93d0cf037ccf4d9ebf406787e94bdac
Nishat Ahmed <nishatahmed4485@gmail.com>
2022-02-08 17:29:44 +0600

    controoler route setup

--- controllers/bookControllers.js
+++ controllers/bookControllers.js
@@ -1,34 +1,39 @@
+const asyncHandler = require('express-async-handler')
+
 // Get Books
 // @route GET/api/books
 // @access private
 
-const getBooks = (req,res)=>{
+const getBooks = asyncHandler( async(req,res)=>{
 
     res.status(200).json({message: 'Get Books'})
-}
+})
 
 // Set Book
 //@route POST/api/books
 // @access private
 
-const setBook = (req,res)=>{
-    console.log(req.body)
-    
+const setBook =asyncHandler( async(req,res)=>{
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('please add a text field')
+    }
+
     res.status(200).json({message: 'Set Book'})
-}
+})
 // Update Books
 //@route PUT/api/books/:id
 // @access private
 
-const updateBook = (req,res)=>{
+const updateBook = asyncHandler( async(req,res)=>{
     res.status(200).json({message: `Update Book cbk${req.params.id}`})
-}
+})
 // Delete Book
 // @route  DELETE/api/books/:id
 // @access private
-const deleteBook = (req,res)=>{
+const deleteBook =asyncHandler( async(req,res)=>{
     res.status(200).json({message: `Delete Book ${req.params.id}`})
-}
+})
 
 module.exports ={
     getBooks,
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express= require('express');
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 
@@ -9,6 +10,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.use('/api/books', require('./routes/bookRoutes'));
+
+app.use(errorHandler)
+
 
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
