53659cb88c3987e0cb55a162b1dfc9945658ed42
Nishat Ahmed <nishatahmed4485@gmail.com>
2022-02-10 12:12:07 +0600

    crud

--- controllers/bookControllers.js
+++ controllers/bookControllers.js
@@ -1,12 +1,16 @@
 const asyncHandler = require('express-async-handler')
 
+const Book = require('../models/bookModal')
+
+
 // Get Books
 // @route GET/api/books
 // @access private
 
 const getBooks = asyncHandler( async(req,res)=>{
+const books = await Book.find()
 
-    res.status(200).json({message: 'Get Books'})
+    res.status(200).json(books)
 })
 
 // Set Book
@@ -18,21 +22,41 @@ const setBook =asyncHandler( async(req,res)=>{
         res.status(400)
         throw new Error('please add a text field')
     }
-
-    res.status(200).json({message: 'Set Book'})
+const book = await Book.create({
+    text: req.body.text
+})
+    res.status(200).json()
 })
 // Update Books
 //@route PUT/api/books/:id
 // @access private
 
 const updateBook = asyncHandler( async(req,res)=>{
-    res.status(200).json({message: `Update Book cbk${req.params.id}`})
+    const book = await Book.findById(req.params.id)
+if(!book){
+    res.status(400)
+    throw new Error('Book not found')
+
+}
+   const updateBook = await Book.findByIdAndUpdate(req.params.id, req.
+    body,{
+        new:true,
+
+    })
+    res.status(200).json(updateBook)
 })
 // Delete Book
 // @route  DELETE/api/books/:id
 // @access private
 const deleteBook =asyncHandler( async(req,res)=>{
-    res.status(200).json({message: `Delete Book ${req.params.id}`})
+    const book = await Book.findById(req.params.id)
+    if(!book){
+        res.status(400)
+        throw new Error('Book not found')
+    }
+  await book.remove()
+
+    res.status(200).json({ id:req.params.id})
 })
 
 module.exports ={
